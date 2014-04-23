6add3a0878c26d09205e8e9e5c82cba04984a39c
Aaron Gable <agable@google.com>
2014-04-23 16:22:33 -0700

    Add zshenv to zsh section.

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,5 +4,6 @@ sudo apt-get install zsh
 sudo chsh /bin/zsh
 
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zsh/zshenv ~/.zshenv
 ln -sf ~/dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/dotfiles/zsh/custom/* ~/.oh-my-zsh/custom
