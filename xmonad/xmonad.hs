6fd00503eb14bbc0eccdf0ae388b3babc55bd8ff
Aaron Gable <aarongable@gmail.com>
2014-07-21 18:15:10 -0700

    Switch xmonad from WindowNavigation to Navigation2D

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,4 +1,16 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{- |
+Module       : xmonad.hs
+Description  : Configuration for the XMonad window manager.
+Copyright    : (c) Aaron Gable (aarongable@gmail.com)
+License      : Fork me on GitHub
+
+Maintainer   : aarongable@gmail.com
+Stability    : experimental
+Portability  : non-portable
+
+My personal XMonad configuration, intended to run standalone on Ubuntu 14.04.
+-}
 
 -- | Imports
 import XMonad hiding ((|||))
@@ -13,13 +25,14 @@ import XMonad.Util.EZConfig
 import System.Exit
 import System.IO
 
+-- Actions
+import XMonad.Actions.Navigation2D
+
 -- Imports for various layouts
-import XMonad.Actions.WindowNavigation
 import XMonad.Layout hiding ((|||))
 import XMonad.Layout.LayoutCombinators
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.ThreeColumns
-import XMonad.Layout.Spiral
 import XMonad.Layout.Tabbed
 
 -- Imports for desktop management
@@ -30,8 +43,6 @@ import XMonad.Hooks.ManageDocks
 import qualified XMonad.Util.ExtensibleState as XS
 -- * for executing actions on both screens without changing focus
 import XMonad.Actions.OnScreen
--- * for alt-tab functionality between screens and workspaces
-import XMonad.Actions.CycleWS
 
 
 -- TODO(aarongable):
@@ -42,24 +53,25 @@ import XMonad.Actions.CycleWS
 
 
 -- | Main config
-main = do
-  config <- withWindowNavigation (xK_k, xK_h, xK_j, xK_l) $ defaultConfig {
-      terminal            = myTerminal
-    , modMask             = mod4Mask
-    , focusFollowsMouse   = False
-    , workspaces          = myWorkspaces
-    , layoutHook          = myLayoutHook
-    , manageHook          = myManageHook
-    , keys                = myKeys
-    , borderWidth         = myBorderWidth
-    , normalBorderColor   = myInactiveBorderColor
-    , focusedBorderColor  = myActiveBorderColor
+main = xmonad $ withNavigation2DConfig defaultNavigation2DConfig
+              $ defaultConfig
+  { terminal            = myTerminal
+  , modMask             = myModMask
+  , focusFollowsMouse   = myFocus
+  , workspaces          = myWorkspaces
+  , layoutHook          = myLayoutHook
+  , manageHook          = myManageHook
+  , keys                = myKeys
+  , borderWidth         = myBorderWidth
+  , normalBorderColor   = myInactiveBorderColor
+  , focusedBorderColor  = myActiveBorderColor
   }
-  xmonad config
 
 
--- | Terminal
+-- | Basics
 myTerminal = "gnome-terminal"
+myModMask = mod4Mask
+myFocus = False
 
 
 -- | Workspaces
@@ -75,88 +87,88 @@ myFunKeys = map (\n -> "<F"++n++">") (map show [1..10])
 -- windowNavigation for M-[hjkl] movement
 -- avoidStrutsOn [] to get toggleStruts, but hiding panels by default
 myLayoutHook = avoidStrutsOn []
-  ( two ||| Mirror two ||| ThreeCol 1 (3/100) (1/3) ||| spiral (1) ||| simpleTabbed )
+  ( two ||| Mirror two ||| ThreeCol 1 (3/100) (1/3) |||  simpleTabbed )
     where two = ResizableTall 1 (3/100) (1/2) []
 
 
 -- | Management
-myManageHook = composeAll
-    [ className =? "Xmessage" --> doFloat
-    , manageDocks
-    ]
+myManageHook = composeAll [
+    className =? "Xmessage" --> doFloat
+  , manageDocks
+  ]
 
 
 -- | Keyboard shortcuts
+myNavKeys = ["h", "j", "k", "l"]
+myNavKeys2 = ["y", "i", "o", "p"]
+myNavDirs = [L, D, U, R]
+
+myNavigation2DConfig = defaultNavigation2DConfig {
+
+}
+
 myKeys = \conf -> mkKeymap conf $
-  -- Most window movement is handed by XMonad.Actions.WindowNavigation
-  [
-  -- Navigate down/up the window stack (useful in tabbed layout)
+  [ -- Directional window navigation (Navigation2D)
+    ("M-h",             windowGo L False)
+  , ("M-j",             windowGo D False)
+  , ("M-k",             windowGo U False)
+  , ("M-l",             windowGo R False)
+  , ("M-S-h",           windowSwap L False)
+  , ("M-S-j",           windowSwap D False)
+  , ("M-S-k",           windowSwap U False)
+  , ("M-S-l",           windowSwap R False)
+  , ("M-S-y",           windowToScreen L False)
+  , ("M-S-u",           windowToScreen D False)
+  , ("M-S-i",           windowToScreen U False)
+  , ("M-S-o",           windowToScreen R False)
+  , ("M-y",             screenGo L False)
+  , ("M-u",             screenGo D False)
+  , ("M-i",             screenGo U False)
+  , ("M-o",             screenGo R False)
+  , ("M1-S-y",          screenSwap L True)
+  , ("M1-S-u",          screenSwap D True)
+  , ("M1-S-i",          screenSwap U True)
+  , ("M1-S-o",          screenSwap R True)
+  ]
+  ++
+  [ -- Navigation utilities
     ("M-<Tab>",         windows W.focusDown)
   , ("M-S-<Tab>",       windows W.focusUp  )
-
-  -- Managing master panes.
   , ("M-m",             windows W.focusMaster)
   , ("M-S-m",           windows W.swapMaster)
-
-  -- Drop floating window back into tiling
   , ("M-t",             withFocused $ windows . W.sink)
-
-  -- Kill window
   , ("M-q",             kill)
   ]
-
-  -- Layout Management
   ++
-  [ ("M-<Space>",       sendMessage NextLayout)
+  [ -- Layout Management
+    ("M-<Space>",       sendMessage NextLayout)
   , ("M-S-<Space>",     sendMessage FirstLayout)
-
   , ("M--",             sendMessage (IncMasterN (-1)))
   , ("M-=",             sendMessage (IncMasterN 1))
-
   , ("M-S--",           sendMessage Shrink)
   , ("M-S-=",           sendMessage Expand)
-
   , ("M-b",             sendMessage ToggleStruts)
-
   , ("M-<Scroll_lock>", XS.modify wsTogglePairState)
   ]
-
-  -- Applications
   ++
-  [ ("M-<Return>",      spawn $ XMonad.terminal conf)
-                                -- colors chosen to match Ubuntu 12.04
-  , ("M-S-<Return>",    spawn $ "dmenu_run -nb '#2C001E' -nf '#AEA79F'"
-                                        ++ " -sb '#AEA79F' -sf '#2C001E'"
-                                        ++ " -l 4")
+  [ -- Applications
+    ("M-<Return>",      spawn $ XMonad.terminal conf)
+  , ("M-S-<Return>",    spawn $ "dmenu_run -l 4 -nb '#2C001E' -nf '#AEA79F'"
+                                           ++ " -sb '#AEA79F' -sf '#2C001E'")
   , ("<F12>",           spawn $ "gnome-screensaver-command --lock")
   , ("<Print>",         spawn $ "gnome-screenshot --interactive")
   ]
-
-  -- XMonad system
   ++
-  [ ("M-C-<Esc>",       spawn $ "xkill")
+  [ -- XMonad system
+    ("M-C-<Esc>",       spawn $ "xkill")
   , ("M-S-q",           io (exitWith ExitSuccess))
   , ("M-S-r",           spawn "xmonad --recompile; xmonad --restart")
   ]
-
-  -- Shift monitors
-    -- mod-{y/u,o/i}: Switch to physical/Xinerama screens 1, 2
-    -- mod-shift-{y/u,o/i}: Move window to screen 1, 2
   ++
-  [ (m ++ key, screenWorkspace sc >>= flip whenJust (windows . f))
-  | (key, sc) <- zip ["y", "u", "o", "i"] [0, 0, 1, 1]
-  , (m, f) <- [("M-", W.view), ("M-S-", W.shift)]
-  ]
-  -- alt-tab for workspaces, via CycleWS
-  ++
-  [ ("M-`",             toggleWS)
-  ]
-
-  -- Unified workspace shifting
-  ++
-  [ (m ++ k,            f k)
-  | k <- myWsKeys
-  , (m, f) <- [("M-", myViewer), ("M-S-", myShifter)]
+  [ -- Unified workspace shifting
+    (m ++ k,            f k)
+    | k <- myWsKeys
+    , (m, f) <- [("M-", myViewer), ("M-S-", myShifter)]
   ]
 
 
@@ -195,4 +207,3 @@ keyWs k = snd . head $ filter ((==k) . fst) myWsMap
 numKeyWs k = keyWs $ myNumKeys !! modIndex k
 funKeyWs k = keyWs $ myFunKeys !! modIndex k
 modIndex k = M.fromJust (L.elemIndex k myWsKeys) `mod` 10
-
