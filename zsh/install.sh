041b64b0918145bfa864993bba23cecc770135ae
Aaron Gable <aarongable@gmail.com>
2014-08-06 12:32:52 -0700

    Add zlogin file with compositor and desktop background.

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,5 +5,6 @@ sudo chsh /bin/zsh
 
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/zsh/zshenv ~/.zshenv
+ln -sf ~/dotfiles/zsh/zlogin ~/.zlogin
 ln -sf ~/dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/dotfiles/zsh/custom/* ~/.oh-my-zsh/custom
