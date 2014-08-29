fcc19f4f33a9578efc0163d464c2393ba8d48bc3
Aaron Gable <agable@chromium.org>
2014-08-29 15:49:27 -0700

    Install the google cloud sdk, and fix paths referencing it.

--- misc/install.sh
+++ misc/install.sh
@@ -2,3 +2,6 @@
 
 mkdir -p ~/.config/gtk-3.0
 ln -s fix-f10-in-gnome.txt ~/.config/gtk-3.0/gtk.css
+
+# Install the google-cloud-sdk. Make sure to install in in $HOME/bin/
+curl https://sdk.cloud.google.com | bash
