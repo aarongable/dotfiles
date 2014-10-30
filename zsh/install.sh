d4ca71402a193fb302ecd1248d9cedc07a67f858
Aaron Gable <aarongable@gmail.com>
2014-10-30 11:32:22 -0700

    Fix zsh symlink installation.

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,6 +8,13 @@ ln -sfT ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sfT ~/dotfiles/zsh/zshenv ~/.zshenv
 ln -sfT ~/dotfiles/zsh/zlogin ~/.zlogin
 ln -sfT ~/dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
-ln -sfT ~/dotfiles/zsh/custom/*.zsh ~/.oh-my-zsh/custom
-ln -sfT ~/dotfiles/zsh/custom/themes/*.zsh-theme ~/.oh-my-zsh/custom/themes/
-ln -sfT ~/dotfiles/zsh/custom/plugins/* ~/.oh-my-zsh/custom/plugins/
+for z in `ls ~/dotfiles/zsh/custom/*.zsh`; do
+  ln -sfT ~/dotfiles/zsh/custom/$z ~/.oh-my-zsh/custom/$(basename $z)
+done
+for t in `ls ~/dotfiles/zsh/custom/themes`; do
+  ln -sfT ~/dotfiles/zsh/custom/themes/$t ~/.oh-my-zsh/custom/themes/$t
+done
+for p in `ls ~/dotfiles/zsh/custom/plugins`; do
+  ln -sfT ~/dotfiles/zsh/custom/plugins/$p ~/.oh-my-zsh/custom/plugins/$p
+done
+
