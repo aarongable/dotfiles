49b9e5412273f68880acff9d632153d8eb23532a
Aaron Gable <agable@chromium.org>
2018-03-01 11:56:25 -0800

    Simplify tmux+powerline integration

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install tmux
+sudo apt-get install tmux fonts-powerline
 
 ln -sfT ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -sfT ~/dotfiles/tmux/tmux-powerlinerc ~/.tmux-powerlinerc
