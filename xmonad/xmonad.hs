e355e3147927d5d9f98849358cfed3ce16e9ed8d
Aaron Gable <aaron@aarongable.com>
2020-05-21 16:06:25 -0700

    Update xmonad shortcuts

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -220,11 +220,12 @@ myKeys n = \conf -> mkKeymap conf $
   ++
   [ -- Applications
     ("M-<Return>",      spawn $ XMonad.terminal conf)
+  , ("M-p",             spawn $ "code")
   , ("M-S-<Return>",    spawn $ "dmenu_run -l 4 -nb '#2C001E' -nf '#AEA79F'"
                                            ++ " -sb '#AEA79F' -sf '#2C001E'"
                                            ++ " -fn 'DejaVu Sans Mono Book'")
   , ("<F12>",           spawn $ "i3lock -c 2C001E")
-  , ("<Print>",         spawn $ "gnome-screenshot --interactive")
+  , ("<Print>",         spawn $ "gnome-screenshot --area")
   ]
   ++
   [ -- XMonad system
