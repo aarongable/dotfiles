9f73b70267fe6950dc7379f66a622309137c2b14
Aaron Gable <aaron@aarongable.com>
2020-05-21 16:06:20 -0700

    Streamline zsh config

--- zsh/custom/mods.zsh
+++ zsh/custom/mods.zsh
@@ -5,11 +5,3 @@ cd() {
 }
 
 source /usr/share/autojump/autojump.zsh
-
-rdtun() {
-  ssh agable@ssh.chromium.org -L3389:$1.golo.chromium.org:3389
-}
-
-rdspawn() {
-  rdesktop localhost -u GOLO\\agable -p - -5 -K -r clipboard:CLIPBOARD
-}
