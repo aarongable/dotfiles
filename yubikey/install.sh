097d7b940294337150907b6a0b7aed70f039bca4
Aaron Gable <aaron@aarongable.com>
2020-06-18 14:04:48 -0700

    Add setup for yubikey-agent

---
+++ yubikey/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+THIS_DIR=`dirname "$(readlink -f "$0")"`
+
+# Get and install the latest version of yubikey-agent
+go get filippo.io/yubikey-agent
+go install filippo.io/yubikey-agent
+
+# Install the agent binary and its service configuration
+mkdir -p $XDG_CONFIG_HOME/systemd/user
+sudo cp $HOME/go/bin/yubikey-agent /usr/local/bin/
+sudo ln -s $THIS_DIR/yubikey-agent-launch /usr/local/bin/
+ln -s $THID_DIR/yubikey-agent.service $XDG_CONFIG_HOME/systemd/user/
+sudo sed -i 's/use-ssh-agent/use-yubikey-agent/' /etc/X11/Xsession.options
