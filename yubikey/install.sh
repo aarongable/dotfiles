dbc7b70ae24209b4a9bc5267bf601079c15e4da7
Aaron Gable <aaron@aarongable.com>
2020-08-26 10:28:29 -0700

    Fix yubikey install script

--- yubikey/install.sh
+++ yubikey/install.sh
@@ -2,7 +2,7 @@
 
 THIS_DIR=`dirname "$(readlink -f "$0")"`
 
-sudo apt install ykman opensc libpcsclite-dev pcscd
+sudo apt install opensc libpcsclite-dev pcscd
 
 # Get and install the latest version of yubikey-agent
 go get filippo.io/yubikey-agent
@@ -12,6 +12,7 @@ go install filippo.io/yubikey-agent
 mkdir -p $XDG_CONFIG_HOME/systemd/user
 sudo cp $HOME/go/bin/yubikey-agent /usr/local/bin/
 sudo ln -s $THIS_DIR/yubikey-agent-launch /usr/local/bin/
-ln -s $THID_DIR/yubikey-agent.service $XDG_CONFIG_HOME/systemd/user/
+ln -s $THIS_DIR/yubikey-agent.service $XDG_CONFIG_HOME/systemd/user/
 systemctl --user enable yubikey-agent
 sudo sed -i 's/use-ssh-agent/use-yubikey-agent/' /etc/X11/Xsession.options
+systemctl --user start yubikey-agent
