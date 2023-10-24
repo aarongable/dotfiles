64fcae6d3f0f2ef731fd83199c2a33386710fe9f
Aaron Gable <aaron@letsencrypt.org>
2023-10-24 15:03:22 -0700

    Simplify zsh install slightly

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,17 +2,14 @@
 
 THIS_DIR=`dirname "$(readlink -f "$0")"`
 
-sudo apt install curl zsh autojump fzf
+sudo apt install curl zsh autojump fzf bat
 chsh -s /usr/bin/zsh
 
 # Install antibody to manage zsh plugins.
 curl -sfL git.io/antibody | sh -s - -b ~/.local/bin
 
 # Install starship to control the prompt.
-curl -fsSL https://starship.rs/install.sh | bash
-
-# Install the google-cloud-sdk. Make sure to install in in $HOME/.local/share/
-curl https://sdk.cloud.google.com | bash
+curl -fsSL https://starship.rs/install.sh | sh
 
 ln -sfT $THIS_DIR/bootstrap ~/.zshenv
 
@@ -21,7 +18,7 @@ ln -sfT $THIS_DIR/zshenv $XDG_CONFIG_HOME/zsh/.zshenv
 ln -sfT $THIS_DIR/zshrc $XDG_CONFIG_HOME/zsh/.zshrc
 ln -sfT $THIS_DIR/aliases.zsh $XDG_CONFIG_HOME/zsh/aliases.zsh
 
-antibody bundle < $THIS_DIR/plugins.txt > $XDG_CACHE_HOME/zsh/plugins.zsh
+~/.local/bin/antibody bundle < $THIS_DIR/plugins.txt > $XDG_CACHE_HOME/zsh/plugins.zsh
 
 # This is where history will be stored.
 mkdir -p $XDG_CACHE_HOME/zsh
