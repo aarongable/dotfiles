9e9b716ab1fc35a4d266bddde8a14bc537cf4b67
Aaron Gable <aaron@letsencrypt.org>
2023-10-24 16:28:03 -0700

    Prevent camera service from trying to start at boot

--- camera/install.sh
+++ camera/install.sh
@@ -5,7 +5,8 @@ THIS_DIR=`dirname "$(readlink -f "$0")"`
 sudo apt install v4l2loopback-dkms v4l2loopback-utils gphoto2 ffmpeg
 
 # Create the systemd service that streams video from the camera
-sudo ln -s $THIS_DIR/dslr-webcam.service /etc/systemd/system/
+sudo ln -sfT $THIS_DIR/dslr-webcam.service /lib/systemd/system/dslr-webcam.service
+sudo systemctl disable dslr-webcam
 
 # Create the udev rule that starts the systemd service when the camera connects
-sudo ln -s $THIS_DIR/dslr-webcam.rules /lib/udev/rules.d/35-dslr-webcam.rules
+sudo ln -sfT $THIS_DIR/dslr-webcam.rules /lib/udev/rules.d/35-dslr-webcam.rules
