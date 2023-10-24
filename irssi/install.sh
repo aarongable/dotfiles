fe3127b32a30a177d75dbe64f83d05ad2dfc370d
Aaron Gable <aaron@letsencrypt.org>
2023-10-24 16:31:12 -0700

    Remove defunct irssi and xmonad dirs

--- irssi/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-THIS_DIR=`dirname "$(readlink -f "$0")"`
-
-sudo apt install irssi
-
-ln -sfT $THIS_DIR ~/.irssi
--- xmonad/screenshot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-maim -s --format png /dev/stdout | xclip -selection clipboard -t image/png -i
-notify-send -u "low" -t 500 "Copied!" "Screenshot copied to clipboard!"
