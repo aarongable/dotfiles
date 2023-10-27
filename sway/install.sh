a0a5e271811d9f80d7797cad3de5026a35b186a5
Aaron Gable <aaron@letsencrypt.org>
2023-10-27 14:26:35 -0700

    Replace mako with sway-notification-center

--- sway/install.sh
+++ sway/install.sh
@@ -5,10 +5,10 @@ THIS_DIR=`dirname "$(readlink -f "$0")"`
 
 sudo apt update
 sudo apt install sway dmenu swaybg swayidle swaylock
-sudo apt install waybar fonts-font-awesome mako-notifier
+sudo apt install waybar fonts-font-awesome sway-notification-center
 sudo apt install brightnessctl pavucontrol
 sudo apt install wl-clipboard grim slurp jq
-sudo apt install alacritty
+sudo apt install kitty
 
 mkdir -p $XDG_CONFIG_HOME/sway
 ln -sfT $THIS_DIR/sway.config $XDG_CONFIG_HOME/sway/config
@@ -20,8 +20,9 @@ ln -sfT $THIS_DIR/waybar.css $XDG_CONFIG_HOME/waybar/style.css
 mkdir -p $XDG_CONFIG_HOME/swaylock
 ln -sfT $THIS_DIR/swaylock.config $XDG_CONFIG_HOME/swaylock/config
 
-mkdir -p $XDG_CONFIG_HOME/mako
-ln -sfT $THIS_DIR/mako.config $XDG_CONFIG_HOME/mako/config
+mkdir -p $XDG_CONFIG_HOME/swaync
+ln -sfT $THIS_DIR/swaync.json $XDG_CONFIG_HOME/swaync/config.json
+ln -sfT $THIS_DIR/swaync.css $XDG_CONFIG_HOME/swaync/style.json
 
 mkdir -p $XDG_DATA_HOME/sway
 ln -sfT $THIS_DIR/wallpaper.jpg $XDG_DATA_HOME/sway/wallpaper.jpg
